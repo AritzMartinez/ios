114c96fbec32f61f0cdb0948e43f5237c5f04f07
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2015-10-28 18:05:47 +0100

    Cargar la primera pregunta

--- FlashCards/FlashCards/ViewController.swift
+++ FlashCards/FlashCards/ViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var etiquetaPregunta: UILabel!
+    @IBOutlet weak var etiquetaRespuesta: UILabel!
+    @IBOutlet weak var etiquetaBien: UILabel!
+    @IBOutlet weak var etiquetaMal: UILabel!
+    @IBOutlet weak var botonBien: UIButton!
+    @IBOutlet weak var botonMal: UIButton!
+
+    // Pregunta actual
+    var aleatorio = 0
+    
+    // Modelo
+    let tarjetas = Tarjetas()
+
+    // Marcador
+    var correctas = 0
+    var incorrectas = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Puesta a cero de los marcadores
+        etiquetaBien.text = String(correctas)
+        etiquetaMal.text = String(incorrectas)
+        
+        // Mostrar una pregunta al azar
+        aleatorio = Int(arc4random_uniform(UInt32(tarjetas.getNumPreguntas())))
+        etiquetaPregunta.text = tarjetas.getPregunta(aleatorio)
+        etiquetaRespuesta.text = ""
+        
+        // Desactivar los botones de respuesta
+        botonBien.enabled = false;
+        botonMal.enabled = false;
+    
     }
 
     override func didReceiveMemoryWarning() {
